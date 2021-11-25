93f6dd511bb421de3033e5ac42d904f475a22425
horiiiiii0329 <85778704+horiiiiii0329@users.noreply.github.com>
2021-11-25 22:52:05 +0900

    solve error in tsconfig and change type on homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,9 +6,10 @@ import Main from "../components/Layout/Main";
 import TopBar from "../components/Layout/TopBar";
 import styles from "../styles/Home.module.scss";
 import WeatherDate from "../components/HomePage/WeatherDate";
-import { useState } from "react";
+import { ReactNode, useState } from "react";
 import Row from "../components/Layout/Row";
 import axios from "axios";
+import { IWeatherCard } from "../type";
 
 const leftNews = ["朝日新聞", "毎日新聞"];
 const rightNews = ["読売新聞", "産経新聞", "日経新聞"];
@@ -24,20 +25,29 @@ const DUMMY_DATA = [
 
 const Home: NextPage = ({
   weatherNews,
-}: // asahiData,
-// mainichiData,
-// yomiuriData,
-// sankeiData,
-// nikkeiData,
-any) => {
+  asahiData,
+  mainichiData,
+  yomiuriData,
+  sankeiData,
+  nikkeiData,
+}:
+  | {
+      weatherNews: IWeatherCard;
+      asahiData: object[];
+      mainichiData: object[];
+      yomiuriData: object[];
+      sankeiData: object[];
+      nikkeiData: object[];
+    }
+  | any) => {
   const [leftIsOpen, setLeftIsOpen] = useState(true);
   const [rightIsOpen, setRightIsOpen] = useState(true);
 
-  const [asahiData, setAsahiData] = useState(DUMMY_DATA);
-  const [mainichiData, setMainichiData] = useState(DUMMY_DATA);
-  const [nikkeiData, setNikkeiData] = useState(DUMMY_DATA);
-  const [sankeiData, setSankeiData] = useState(DUMMY_DATA);
-  const [yomiuriData, setYomiuriData] = useState(DUMMY_DATA);
+  // const [asahiData, setAsahiData] = useState(DUMMY_DATA);
+  // const [mainichiData, setMainichiData] = useState(DUMMY_DATA);
+  // const [nikkeiData, setNikkeiData] = useState(DUMMY_DATA);
+  // const [sankeiData, setSankeiData] = useState(DUMMY_DATA);
+  // const [yomiuriData, setYomiuriData] = useState(DUMMY_DATA);
 
   const [leftPickedNews, setLeftPickedNews] = useState<object[]>([]);
   const [rightPickedNews, setRightPickedNews] = useState<object[]>([]);
@@ -138,36 +148,36 @@ export const getStaticProps: GetStaticProps = async (context) => {
   const weatherNews = weatherJson;
 
   // get news list
-  // const mData = await axios.get(
-  //   "https://10x4sx0ksf.execute-api.us-east-1.amazonaws.com/default/fetchMainichi"
-  // );
+  const mData = await axios.get(
+    "https://10x4sx0ksf.execute-api.us-east-1.amazonaws.com/default/fetchMainichi"
+  );
 
-  // const yData = await axios.get(
-  //   "https://dfidli0e6a.execute-api.us-east-1.amazonaws.com/default/fetchYomiuriData"
-  // );
+  const yData = await axios.get(
+    "https://dfidli0e6a.execute-api.us-east-1.amazonaws.com/default/fetchYomiuriData"
+  );
 
-  // const aData = await axios.get(
-  //   "https://364do95wh5.execute-api.us-east-1.amazonaws.com/default/fetchAsahiData"
-  // );
+  const aData = await axios.get(
+    "https://364do95wh5.execute-api.us-east-1.amazonaws.com/default/fetchAsahiData"
+  );
 
-  // const sData = await axios.get(
-  //   "https://69y7orpkvf.execute-api.us-east-1.amazonaws.com/default/fetchSankei"
-  // );
+  const sData = await axios.get(
+    "https://69y7orpkvf.execute-api.us-east-1.amazonaws.com/default/fetchSankei"
+  );
 
-  // const nData = await axios.get(
-  //   "https://oz0czga9rj.execute-api.us-east-1.amazonaws.com/default/nikkeiData"
-  // );
+  const nData = await axios.get(
+    "https://oz0czga9rj.execute-api.us-east-1.amazonaws.com/default/nikkeiData"
+  );
 
   Promise.all([aData, mData, yData, sData, nData]);
 
   return {
     props: {
       weatherNews,
-      // asahiData: aData.data.Items,
-      // mainichiData: mData.data.Items,
-      // yomiuriData: yData.data.Items,
-      // sankeiData: sData.data.Items,
-      // nikkeiData: nData.data.Items,
+      asahiData: aData.data.Items,
+      mainichiData: mData.data.Items,
+      yomiuriData: yData.data.Items,
+      sankeiData: sData.data.Items,
+      nikkeiData: nData.data.Items,
     },
     // revalidate: 60 * 60 * 24,
   };
