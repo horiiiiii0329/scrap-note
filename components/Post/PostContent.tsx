a829a6d6147d5119850f5fe136a91973e764c96b
horiiiiii0329 <85778704+horiiiiii0329@users.noreply.github.com>
2021-12-05 19:28:54 +0900

    implement render tiptap editopr

--- components/Post/PostContent.tsx
+++ components/Post/PostContent.tsx
@@ -3,7 +3,6 @@ import React, { useState } from "react";
 import { useRouter } from "next/router";
 import { supabase } from "../../api";
 import "easymde/dist/easymde.min.css";
-import dynamic from "next/dynamic";
 import { v4 as uuid } from "uuid";
 import {
   useEditor,
@@ -21,15 +20,53 @@ import Giphy from "./giphy/Giphy";
 import Modal from "react-modal";
 import CustomImage from "./extensions/image";
 
-const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
-  ssr: false,
-});
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    border: "none",
+    backgroundColor: "transparent",
+    padding: "0px",
+  },
+};
+
 const initialState = { title: "", content: "", id: "" };
 
 function PostContent() {
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  const editor = useEditor({
+    extensions: [
+      StarterKit,
+      Link,
+      TextAlign.configure({ types: ["paragraph", "image"] }),
+      CustomImage.configure({
+        HTMLAttributes: {
+          class: "custom-image",
+        },
+      }),
+    ],
+
+    content: `
+    `,
+  });
+
   const [post, setPost] = useState(initialState);
   const { title, content } = post;
   const router = useRouter();
+
   function onChange(e: React.ChangeEvent<HTMLInputElement>) {
     setPost(() => ({ ...post, [e.target.name]: e.target.value }));
   }
@@ -54,10 +91,36 @@ function PostContent() {
         value={post.title}
         className={styles.input_title}
       />
-      <SimpleMDE
-        value={post.content}
-        onChange={(value) => setPost({ ...post, content: value })}
-      />
+      <EditorContent editor={editor} />
+      {editor && (
+        <BubbleMenu
+          className="bubble-menu"
+          tippyOptions={{ duration: 100 }}
+          editor={editor}
+        >
+          <PopupMenu editor={editor} />
+        </BubbleMenu>
+      )}
+
+      {editor && (
+        <FloatingMenu editor={editor} tippyOptions={{ duration: 100 }}>
+          <div style={{ position: "absolute", top: -15, left: -55 }}>
+            <SideMenu
+              position={{}}
+              editor={editor}
+              display={true || "displaySidebar"}
+              gifClickHandler={openModal}
+            />
+          </div>
+        </FloatingMenu>
+      )}
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+      >
+        <Giphy editor={editor} closeModalHandler={() => setIsOpen(false)} />
+      </Modal>
       <button
         type="button"
         className={styles.button__md}
